f6184ce351d23aa60e04339a5f20e8442f048165
Fidel Leal <fidelxleal@gmail.com>
2022-11-23 10:23:30 -0600

    fix screen scaling issue

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function divide(x, y) {
 function operate(operator, x, y) {
     return operator(x,y);
 }
+
+const screen = document.querySelector('.screen');
+let displayValue = '';
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('click', (e) => displayUpdate(`${key.innerHTML}`)));
+
+// TODO: Create a function that takes the operator and routes the computation to the right function while storing the intermediate
+//result
+
+//TODO: Maybe the screen numbers are a div inside the screen div?
+
+function displayUpdate(val) {
+    displayValue = displayValue + val;
+    screen.innerHTML = displayValue;
+    console.log(displayValue);
+}
